00d8feae01b23e402d69251b69db670a2fc57d64
Sahil Soni <sscsps@gmail.com>
2016-09-11 18:16:53 +0530

    a few changes(login broken)

--- StudentPortal/Index.aspx.cs
+++ StudentPortal/Index.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace StudentPortal
 {
-    public partial class _Default : Page
+    public partial class Index : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
