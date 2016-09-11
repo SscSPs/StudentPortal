10c92a7724a456f0f2ad9d2282dc9b215fe86e28
Sahil Soni <sscsps@gmail.com>
2016-09-11 16:59:07 +0530

    a few changes

--- StudentPortal/Login.aspx.cs
+++ StudentPortal/Login.aspx.cs
@@ -27,14 +27,14 @@ namespace StudentPortal
             string path = MapPath(null);
             string constring = ConfigurationManager.ConnectionStrings["dbconnectionPart1"].ConnectionString + path + ConfigurationManager.ConnectionStrings["dbconnectionPart2"].ConnectionString;
             SqlConnection con = new SqlConnection(constring);
-            con.Open();
+            con.Open(); ;
             SqlCommand cmd = new SqlCommand("select * from LoginTable where Uname=@username and Pword=@password", con);
             cmd.Parameters.AddWithValue("@username", Uname.Text);
             cmd.Parameters.AddWithValue("@password", Pword.Text);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
-            if (dt.Rows.Count > 0)
+            if (dt.Rows.Count == 0)
             {
                 Response.Redirect("About.aspx");
             }
