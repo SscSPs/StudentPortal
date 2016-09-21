68f8103e6e7e627b4f3ce5bcb77ab6e07b7940dd
Sahil Soni <sscsps@gmail.com>
2016-09-21 14:47:43 +0530

    batch added to student details

--- StudentPortal/Registration.aspx.cs
+++ StudentPortal/Registration.aspx.cs
@@ -1,15 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace StudentPortal
 {
     public partial class Registration : System.Web.UI.Page
     {
+        static string path;
+        static string constring;
+        SqlConnection con;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            path = Server.MapPath(null);
+            constring = ConfigurationManager.ConnectionStrings["dbconnectionPart1"].ConnectionString + path + ConfigurationManager.ConnectionStrings["dbconnectionPart2"].ConnectionString;
+            con = new SqlConnection(constring);
+
+
+            Reg_inp_batch.Items.Clear();
+            string batchquery = "select BatchName from Batch";
+            List<string> batches = Reader(batchquery);
+            foreach(string a in batches)
+            Reg_inp_batch.Items.Add(a);
+        }
+
+        private List<string> Reader(string sqCommand)
+        {
+            con.Open();
+            using (SqlCommand cmd = new SqlCommand(sqCommand, con))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                List<string> list = new List<string>();
+                while (reader.Read())
+                {
+                    string av = null;
+                    av = reader[0].ToString();
+                    list.Add(av);
+                }
+                return list;
+            }
         }
 
         protected void ButtRegis_click(object sender, EventArgs e)
@@ -19,9 +51,6 @@ namespace StudentPortal
             {             
                 try
                 {
-                    string path = Server.MapPath(null);
-                    string constring = ConfigurationManager.ConnectionStrings["dbconnectionPart1"].ConnectionString + path + ConfigurationManager.ConnectionStrings["dbconnectionPart2"].ConnectionString;
-                    SqlConnection con = new SqlConnection(constring);
                     con.Open();
                     string cmd1, cmd2;
                     cmd1 = "Insert into LoginTable values('"
@@ -31,7 +60,8 @@ namespace StudentPortal
                         + Reg_inp_Rollnumber.Text
                         + "', '" + Reg_inp_fname.Text
                         + "', '" + Reg_inp_lname.Text
-                        + "', '" + Reg_inp_email.Text + "')";
+                        + "', '" + Reg_inp_email.Text
+                        + "', '" + Reg_inp_batch.Text + "')";
                     SqlCommand cmd = new SqlCommand(cmd1, con);
                     cmd.ExecuteNonQuery();
                     cmd = new SqlCommand(cmd2, con);
--- StudentPortal/Workspace.aspx
+++ StudentPortal/Workspace.aspx
@@ -1,8 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Workspace.aspx.cs" Inherits="StudentPortal.Workspace" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <div id="work_details">
-    <asp:Label ID="Label1" runat="server" Text="Attendance: "></asp:Label>
-    <asp:Label ID="Label2" runat="server" Text="85%"></asp:Label><br /><br />
-    <asp:Label ID="Label3" runat="server" Text="SGPA: "></asp:Label><asp:Label ID="Label4" runat="server" Text="8.23"></asp:Label>
-        </div>
+        <asp:Label ID="Label1" runat="server" Text="Attendance: "></asp:Label>
+        <asp:Label ID="Label2" runat="server" Text="85%"></asp:Label><br /><br />
+        <asp:Label ID="Label3" runat="server" Text="SGPA: "></asp:Label><asp:Label ID="Label4" runat="server" Text="8.23"></asp:Label>
+    </div>
 </asp:Content>
